8bd648ba584b6ae492b26bf42531229ddadae30c
Pravallika11112345678 <pravallikauddarraju@gmail.com>
2025-10-25 20:59:38 +0530

    updated....

--- LibraryManagement.cpp
+++ LibraryManagement.cpp
@@ -1,214 +1,173 @@
-#include <stdio.h>
-#include <fstream>
 #include <iostream>
-#include <conio.h>
-#include <string.h>
-
+#include <fstream>
+#include <cstring>
 using namespace std;
 
+class Book {
+private:
+    int bookId;
+    char title[50];
+    float price;
 
-class Book
-{
-	private :
-		int bookId;
-		char title[20];
-		float price;
+public:
+    Book() {
+        bookId = 0;
+        strcpy(title, "No Name");
+        price = 0;
+    }
 
-	public :
-		Book()
-		{
-			bookId= 0;
-			strcpy(title,"no name");
-			price = 0;
-		}
-		void getBookData()
-		{
-			cout<<"\nEnter Book Id : ";
-			cin>>bookId;
-			cin.ignore();
-			cout<<"Enter Book Name : ";
-			cin.getline(title,20);
-			cout<<"Enter Book Cost : ";
-			cin>>price;
-		}
-		void showBookData()
-		{
-			cout<<"\n\nBook Id : "<<bookId<<"\nBook Title : "<<title<<"\nBook Price : "<<price;
-		}
-		void storeBook(void);
-		void viewAllBook(void);
-		void searchBook(char *);
-		void delBook(char *);
-		void updateBook(char *);
+    void getBookData() {
+        cout << "\nEnter Book ID: ";
+        cin >> bookId;
+        cin.ignore(); // ignore newline after number
+        cout << "Enter Book Title: ";
+        cin.getline(title, 50);
+        cout << "Enter Book Price: ";
+        cin >> price;
+    }
+
+    void showBookData() const {
+        cout << "\nBook ID: " << bookId
+             << "\nBook Title: " << title
+             << "\nBook Price: " << price << endl;
+    }
+
+    void storeBook() {
+        if (bookId == 0 && price == 0) {
+            cout << "\nInvalid Book Details!";
+            return;
+        }
+        ofstream fout("books.dat", ios::app | ios::binary);
+        fout.write(reinterpret_cast<char*>(this), sizeof(*this));
+        fout.close();
+        cout << "\nBook added successfully!\n";
+    }
+
+    void viewAllBooks() {
+        ifstream fin("books.dat", ios::in | ios::binary);
+        if (!fin) {
+            cout << "\nNo books found.\n";
+            return;
+        }
+        Book b;
+        while (fin.read(reinterpret_cast<char*>(&b), sizeof(b))) {
+            b.showBookData();
+        }
+        fin.close();
+    }
+
+    void searchBook(const char* searchTitle) {
+        ifstream fin("books.dat", ios::in | ios::binary);
+        if (!fin) {
+            cout << "\nNo books found.\n";
+            return;
+        }
+        Book b;
+        bool found = false;
+        while (fin.read(reinterpret_cast<char*>(&b), sizeof(b))) {
+            if (strcmp(b.title, searchTitle) == 0) {
+                b.showBookData();
+                found = true;
+            }
+        }
+        if (!found) cout << "\nBook not found.\n";
+        fin.close();
+    }
+
+    void deleteBook(const char* delTitle) {
+        ifstream fin("books.dat", ios::in | ios::binary);
+        ofstream fout("temp.dat", ios::out | ios::binary);
+        Book b;
+        bool found = false;
+        while (fin.read(reinterpret_cast<char*>(&b), sizeof(b))) {
+            if (strcmp(b.title, delTitle) != 0) {
+                fout.write(reinterpret_cast<char*>(&b), sizeof(b));
+            } else {
+                found = true;
+                cout << "\nDeleting Book: " << b.title << endl;
+            }
+        }
+        fin.close();
+        fout.close();
+        remove("books.dat");
+        rename("temp.dat", "books.dat");
+        if (!found) cout << "\nBook not found to delete.\n";
+    }
+
+    void updateBook(const char* updateTitle) {
+        fstream file("books.dat", ios::in | ios::out | ios::binary);
+        Book b;
+        bool found = false;
+        while (file.read(reinterpret_cast<char*>(&b), sizeof(b))) {
+            if (strcmp(b.title, updateTitle) == 0) {
+                cout << "\nOld Book Details:\n";
+                b.showBookData();
+                cout << "\nEnter new details:\n";
+                b.getBookData();
+                file.seekp(-static_cast<int>(sizeof(b)), ios::cur);
+                file.write(reinterpret_cast<char*>(&b), sizeof(b));
+                found = true;
+                cout << "\nBook updated successfully!\n";
+                break;
+            }
+        }
+        if (!found) cout << "\nBook not found to update.\n";
+        file.close();
+    }
 };
 
-void Book :: storeBook(void)
-{
-	if (bookId == 0 && price == 0)
-	{
-		cout<<"\nInvalid Details";
-	}
-	else
-	{
-		ofstream fout;
-		fout.open("book.dat", ios::app|ios::binary);
-		fout.write((char *)this, sizeof(*this));
-		fout.close();
-	}
-}
-void Book:: viewAllBook(void)
-{
-	ifstream fin;
-	fin.open("book.dat", ios::in|ios::binary);
-	if(!fin)
-		cout<<"\nfile not found";
-	else
-	{
-		fin.read((char *)this, sizeof(*this));
-		while(!fin.eof())
-		{
-			showBookData();
-			fin.read((char *)this, sizeof(*this));
-		}
-	}
-	fin.close();
-}
-void Book:: searchBook(char *t)
-{
-	ifstream fin;
-	fin.open("book.dat", ios::in|ios::binary);
-	if(!fin)
-		cout<<"\nfile not found";
-	else
-	{
-		fin.read((char *)this, sizeof(*this));
-		while(!fin.eof())
-		{
-			if(!strcmp(t,title))
-				showBookData();
-			fin.read((char *)this, sizeof(*this));
-		}
-	}
-	fin.close();
-}
+int main() {
+    Book b;
+    int choice;
+    char cont = 'y';
+    char bookTitle[50];
 
-void Book:: delBook(char *t)
-{
-	ifstream fin;
-	ofstream fout;
+    while (cont == 'y' || cont == 'Y') {
+        cout << "\n\n=== Library Management Menu ===";
+        cout << "\n1. Add Book";
+        cout << "\n2. View All Books";
+        cout << "\n3. Search Book";
+        cout << "\n4. Update Book";
+        cout << "\n5. Delete Book";
+        cout << "\n6. Exit";
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+        cin.ignore();
 
-	fin.open("book.dat", ios::in|ios::binary);
-	if(!fin)
-		cout<<"\nfile not found";
-	else
-	{
-		fout.open("temp.dat",ios::out|ios::binary);
-		fin.read((char *)this, sizeof(*this));
-		while(!fin.eof())
-		{
-			if(strcmp(t,title))
-			{
-				showBookData();
-				fout.write((char *)this, sizeof(*this));
-			}
-			fin.read((char *)this, sizeof(*this));
-		}
-	}
-	fin.close();
-	fout.close();
-	remove("book.dat");
-	rename("temp.dat","book.dat");
-	viewAllBook();
-}
+        switch (choice) {
+            case 1:
+                b.getBookData();
+                b.storeBook();
+                break;
+            case 2:
+                b.viewAllBooks();
+                break;
+            case 3:
+                cout << "\nEnter Book Title to Search: ";
+                cin.getline(bookTitle, 50);
+                b.searchBook(bookTitle);
+                break;
+            case 4:
+                cout << "\nEnter Book Title to Update: ";
+                cin.getline(bookTitle, 50);
+                b.updateBook(bookTitle);
+                break;
+            case 5:
+                cout << "\nEnter Book Title to Delete: ";
+                cin.getline(bookTitle, 50);
+                b.deleteBook(bookTitle);
+                break;
+            case 6:
+                cout << "\nThank you for using the library system!\n";
+                return 0;
+            default:
+                cout << "\nInvalid Choice! Please try again.\n";
+        }
 
-void Book:: updateBook(char *t)
-{
-	fstream f;
+        cout << "\nDo you want to continue? (y/n): ";
+        cin >> cont;
+        cin.ignore();
+    }
 
-	f.open("book.dat", ios::in|ios::out|ios::ate|ios::binary);
-	if(!f)
-		cout<<"\nfile not found";
-	else
-	{
-		f.seekg(0);
-		f.read((char *)this, sizeof(*this));
-		while(!f.eof())
-		{
-			if(!strcmp(t,title))
-			{
-				showBookData();
-				getBookData();
-				f.seekp(f.tellp()- (sizeof(*this)));
-				f.write((char *)this, sizeof(*this));
-			}
-			f.read((char *)this, sizeof(*this));
-		}
-	}
-	f.close();
-	viewAllBook();
-}
-
-void main()
-{
-	clrscr();
-	Book b1;
-	int ch;
-	char cont='y';
-	char bb[20];
-
-	while(cont=='y' || cont=='Y')
-	{
-		clrscr();
-		cout<<"\nMain Menu";
-		cout<<"\n1. Add Book Details";
-		cout<<"\n2. View Book Details";
-		cout<<"\n3. View All Book Details";
-		cout<<"\n4. Update Book Details";
-		cout<<"\n5. Remove Book";
-		cout<<"\n6. ...e X i t...";
-		cout<<"\n Enter your choice";
-		cin>>ch;
-
-		switch(ch)
-		{
-			case 1:
-				b1.getBookData();
-				b1.storeBook();
-				break;
-			case 2:
-				b1.viewAllBook();
-				cin.ignore();
-				cout<<"\n\nEnter Book Name : ";
-				cin.getline(bb,20);
-				b1.searchBook(bb);
-				break;
-			case 3:
-				b1.viewAllBook();
-				break;
-			case 4:
-				b1.viewAllBook();
-				cin.ignore();
-				cout<<"\n\nEnter Book Name : ";
-				cin.getline(bb,20);
-				b1.updateBook(bb);
-				break;
-			case 5:
-				b1.viewAllBook();
-				cin.ignore();
-				cout<<"\n\nEnter Book Name : ";
-				cin.getline(bb,20);
-				b1.delBook(bb);
-				break;
-			case 6:
-			       cout<<"\nThanks for visiting ...";
-			       getch();
-			       exit(0);
-			default:
-				cout<<"\nInvalid Choice....";
-		}
-		cout<<"\n\nWant to continue...(y/n)...";
-		cin>>cont;
-
-	}
-	getch();
+    return 0;
 }
